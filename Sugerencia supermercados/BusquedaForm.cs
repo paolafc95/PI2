94286bde8de869316d6f2e71127b3c9e2e42cc29
→∟Paola Fuentes Caro← <Paola Fuentes Caro>
2017-08-13 21:11:25 -0500

    Abrir archivos OK

--- Sugerencia supermercados/BusquedaForm.cs
+++ Sugerencia supermercados/BusquedaForm.cs
@@ -44,31 +44,16 @@ namespace Sugerencia_supermercados
 
         private void abrirdbtn_Click(object sender, EventArgs e)
         {
-            // Displays an OpenFileDialog so the user can select a Cursor.
+            // Displays an OpenFileDialog so the user can select a file.
             OpenFileDialog openFileDialog1 = new OpenFileDialog();
-            openFileDialog1.Filter = "Cursor Files|*.cur";
-            openFileDialog1.Filter = "Archivo|*.txt";
-            openFileDialog1.Title = "Select a Data Base";
-
+            
             // Show the Dialog.
-            // If the user clicked OK in the dialog and a .CUR file was selected, open it.
             if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
-                // Assign the cursor in the Stream to the Form's Cursor property.
-                this.Cursor = new Cursor(openFileDialog1.OpenFile());
-            }
-            /*
-            //abrir un archivo existente
-            if (OpenFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                System.IO.StreamReader sr = new
-                System.IO.StreamReader(OpenFileDialog.FileName);
+                System.IO.StreamReader sr = new System.IO.StreamReader(openFileDialog1.FileName);
                 MessageBox.Show(sr.ReadToEnd());
                 sr.Close();
-            }*/
-
-        }
-
-        
+            }
+        }        
     }
 }
